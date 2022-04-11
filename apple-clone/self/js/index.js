f682a8d96f295387ff6bef8388e4fbae70886c3a
DevFrog92 <yongsu1481@gmail.com>
2022-04-11 12:26:55 +0900

    🌱 add partScroll animation

--- apple-clone/self/js/index.js
+++ apple-clone/self/js/index.js
@@ -6,6 +6,7 @@
   let yOffset = 0; // window.scrollY 대신 사용할 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
   let currentScene = 0; // 현재 활성화된 씬(scroll-section)
+  let enterNewScene = false; // 새로운 scene이 시작된 순간 true
 
   const sceneInfo = [
     {
@@ -21,7 +22,9 @@
         messageD: document.querySelector("#scroll-section-0 .main-message.d"),
       },
       values: {
-        messageA_opacity: [0, 1]
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2}],
+        messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4}],
+        messageA_opacity_out: [0, 1, { start: 0.25, end: 0.3}],
       }
     },
     {
@@ -81,9 +84,28 @@
     // 각 섹션마다 얼마나 스크롤이 되었는지가 중요하다
     let rv;
     // 현재 스크롤 섹션에서 스크롤된 비율 구하기
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight
+    const scrollHeight = sceneInfo[currentScene].scrollHeight
+    const scrollRatio = currentYOffset / scrollHeight;
     // 얼만큼 스크롤 되었는지 비율이 있어야 한다.
-    rv = scrollRatio * (values[1] - values[0]) + values[0]
+
+    if(values.length === 3) {
+      // start ~ end 사이에 애니메이션을 실행
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+      const parScrollRatio = (currentYOffset - partScrollStart) / partScrollHeight
+
+      if(currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
+        rv = parScrollRatio * (values[1] - values[0]) + values[0];
+      }else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      }else if( currentYOffset >= partScrollEnd) {
+        rv = values[1];
+      }
+    }else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+
 
     return rv;
 
@@ -98,8 +120,9 @@
     switch (currentScene) {
       case 0:
 
-        let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset)
+        let messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset)
         objs.messageA.style.opacity = messageA_opacity_in
+        console.log(currentScene, messageA_opacity_in)
         // css setting
         break;
 
@@ -116,6 +139,7 @@
   }
 
   function scrollLoop() {
+    enterNewScene = false;
     prevScrollHeight = 0;
 
     for(let i = 0; i <  currentScene; i++) {
@@ -123,15 +147,21 @@
     }
 
     if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene += 1;
       document.body.setAttribute('id', `show-scene-${currentScene}`)
     }
 
     if(yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if(currentScene === 0) return // 브라우저 바운스 효과 방어
-
       currentScene -= 1;
       document.body.setAttribute('id', `show-scene-${currentScene}`)
+    }
+
+    // scroll의 오동작(-값)을 방어하기 위한 코드
+    if(enterNewScene) {
+      return
     }
 
     playAnimation();
